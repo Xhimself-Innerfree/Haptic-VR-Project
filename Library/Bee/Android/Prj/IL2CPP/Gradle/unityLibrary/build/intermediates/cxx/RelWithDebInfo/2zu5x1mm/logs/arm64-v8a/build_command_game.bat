276132a471406d68e070a897f66437264ce63c1d
Xhimself-Innerfree <xhimselfinnerfree@gmail.com>
2025-04-28 01:42:45 -0400

    rm Library/

--- Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/2zu5x1mm/logs/arm64-v8a/build_command_game.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Unity Editor\\6000.0.45f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\UnityProjects\\haptic-3D-v1\\.utmp\\RelWithDebInfo\\2zu5x1mm\\arm64-v8a" ^
-  game
