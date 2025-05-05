79cac76610d93044884aa10d15d0296fee421694
Xhimself-Innerfree <xhimselfinnerfree@gmail.com>
2025-05-04 21:06:25 -0400

    AINPC

---
+++ Assets/Scripts_X/World_Ctrl/RandomMove.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float range; // Ëæ»úÒÆ¶¯µÄ·¶Î§
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Vector3 randomPoint = transform.position + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                agent.SetDestination(hit.position);
+            }
+        }
+    }
+}
